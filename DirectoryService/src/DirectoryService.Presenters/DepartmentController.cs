c7aca668b9d895bb6207435e367e6244568540b2
Wilmar Barlow <WilmarBarlow@yandex.ru>
2025-11-12 12:23:38 +0300

    Created Contracts, Domain, Presentation and Presenters projects

---
+++ DirectoryService/src/DirectoryService.Presenters/DepartmentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DirectoryService.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet]
+        public void Test()
+        {
+
+        }
+    }
+}
